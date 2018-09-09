00a4b2f6fa71d2a651bc1e2ae3b8ec646b7a89c9
email <phupeter916@gmail.com>
2018-09-08 17:03:09 -0700

    mostcurrent

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,61 +3,61 @@ var friends = [
 
     {
         name:"Ahmed",
-        photo:"http://celebrityinsider.org/wp-content/uploads/2018/09/Brad-Pitt-Angelina-Jolie.jpg",
+        photo:"https://static-cdn.jtvnw.net/jtv_user_pictures/hsdogdog-profile_image-5550ade194780dfc-300x300.jpeg",
         scores:[ 5, 1, 4, 4, 5, 1, 2, 5, 4, 1 ]
     },
 
     {
         name:"Randy",
-        photo:"http://celebrityinsider.org/wp-content/uploads/2018/09/Brad-Pitt-Angelina-Jolie.jpg",
+        photo:"https://static-cdn.jtvnw.net/jtv_user_pictures/hsdogdog-profile_image-5550ade194780dfc-300x300.jpeg",
         scores:[ 5, 2, 4, 3, 5, 1, 4, 4, 3, 1 ]
     },
 
     {
         name:"Carol",
-        photo:"https://media.brides.com/photos/5a0ef9c31378d411e0e3fac8/1:1/w_767/margot-robbie-posts-photo-of-husband-and-puppy.jpg",
+        photo:"https://static-cdn.jtvnw.net/jtv_user_pictures/hsdogdog-profile_image-5550ade194780dfc-300x300.jpeg",
         scores:[ 4, 3, 3, 1, 3, 5, 3, 2, 2, 5 ]
     },
 
     {
         name:"Will",
-        photo:"http://media.aceuniverse.com/pub/media/catalog/product/cache/image/480x680/e9c3970ab036de70892d86c6d221abfe/g/u/guest_biopic-evans_1.jpg",
+        photo:"https://static-cdn.jtvnw.net/jtv_user_pictures/hsdogdog-profile_image-5550ade194780dfc-300x300.jpeg",
         scores:[ 1, 5, 5, 4, 3, 2, 4, 4, 5, 2 ]
     },
 
     {
         name:"Kaylee",
-        photo:"https://media.brides.com/photos/5a0ef9c31378d411e0e3fac8/1:1/w_767/margot-robbie-posts-photo-of-husband-and-puppy.jpg",
+        photo:"https://static-cdn.jtvnw.net/jtv_user_pictures/hsdogdog-profile_image-5550ade194780dfc-300x300.jpeg",
         scores:[ 4, 4, 4, 2, 5, 1, 4, 3, 5, 2 ]
     },
 
     {
         name:"Tina",
-        photo:"https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzkxOTI1MjYy/scarlett-johansson-13671719-1-402.jpg",
+        photo:"https://static-cdn.jtvnw.net/jtv_user_pictures/hsdogdog-profile_image-5550ade194780dfc-300x300.jpeg",
         scores:[ 1, 1, 3, 3, 5, 1, 5, 5, 2, 3 ]
     },
 
     {
         name:"Ashley",
-        photo:"https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzkxOTI1MjYy/scarlett-johansson-13671719-1-402.jpg",
+        photo:"https://static-cdn.jtvnw.net/jtv_user_pictures/hsdogdog-profile_image-5550ade194780dfc-300x300.jpeg",
         scores:[ 1, 3, 3, 4, 5, 2, 5, 5, 3, 1 ]
     },
 
     {
         name:"Thomas",
-        photo:"http://media.aceuniverse.com/pub/media/catalog/product/cache/image/480x680/e9c3970ab036de70892d86c6d221abfe/g/u/guest_biopic-evans_1.jpg",
+        photo:"https://static-cdn.jtvnw.net/jtv_user_pictures/hsdogdog-profile_image-5550ade194780dfc-300x300.jpeg",
         scores:[ 3, 2, 5, 3, 5, 2, 3, 4, 5, 5 ]
     },
 
     {
         name:"Jon",
-        photo:"http://media.aceuniverse.com/pub/media/catalog/product/cache/image/480x680/e9c3970ab036de70892d86c6d221abfe/g/u/guest_biopic-evans_1.jpg",
+        photo:"https://static-cdn.jtvnw.net/jtv_user_pictures/hsdogdog-profile_image-5550ade194780dfc-300x300.jpeg",
         scores:[ 2, 1, 5, 2, 5, 5, 3, 4, 2, 1 ]
     },
 
     {
         name:"Elaine",
-        photo:"https://www.biography.com/.image/t_share/MTE4MDAzNDEwNzkxOTI1MjYy/scarlett-johansson-13671719-1-402.jpg",
+        photo:"https://static-cdn.jtvnw.net/jtv_user_pictures/hsdogdog-profile_image-5550ade194780dfc-300x300.jpeg",
         scores:[ 1, 5, 3, 4, 5, 5, 4, 3, 2, 2 ]
     },
 
